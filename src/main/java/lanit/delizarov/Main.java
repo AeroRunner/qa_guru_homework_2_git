ef1f677350c2e7b8d52fd5c7965951c30dea68b0
AeroRunner <airplay1x6@gmail.com>
2023-10-25 01:19:00 +0300

    Update 1 = new int a = 12;

--- src/main/java/lanit/delizarov/Main.java
+++ src/main/java/lanit/delizarov/Main.java
@@ -3,8 +3,9 @@ package lanit.delizarov;
 
 public class Main {
     public static void main(String[] args) {
-
+         int a = 12;
         System.out.printf("Hello and welcome!");
+        System.out.print(a);
 
 
 
